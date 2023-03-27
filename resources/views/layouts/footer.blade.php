2ee53454ebe0bba69586d1083500cefef02b4679
afifahhasnawaf <afifahha0801@gmail.com>
2023-03-27 19:08:03 +0700

    feat/add landing-page

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,57 @@
+<!-- ======= Footer ======= -->
+<footer id="footer " class="footer ">
+
+<div class="container ">
+    <div class="row gy-4 ">
+        <div class="col-lg-5 col-md-12 footer-info ">
+            <a href="index.html " class="logo d-flex align-items-center ">
+                <span>Tiba-tiba pindahan</span>
+            </a>
+            <p>Titipan adalah layanan jasa pindahan kantor, rumah, gudang terpercaya. Selain pindahan titipan juga menyediakan jasa pengiriman barang ke seluruh Indonesia.</p>
+            <div class="social-links d-flex mt-4 ">
+                <a href="# " class="twitter "><i class="bi bi-tiktok "></i></a>
+                <a href="https://www.instagram.com/tibatibapindahan/?hl=id" class="instagram" target="_blank"><i class="bi bi-instagram "></i></a>
+                <a href="https://www.linkedin.com/company/titipan-logistik/" class="linkedin " target="_blank"><i class="bi bi-linkedin "></i></a>
+            </div>
+        </div>
+
+        <div class="col-lg-2 col-6 footer-links ">
+            <h4>Useful Links</h4>
+            <ul>
+                <li><a href="index.html">Home</a></li>
+                <li><a href="#about">About us</a></li>
+                <li><a href="#service">Services</a></li>
+            </ul>
+        </div>
+
+        <div class="col-lg-2 col-6 footer-links ">
+            <h4>Layanan</h4>
+            <ul>
+                <li><a href="pindahan.html">Jasa Pindahan</a></li>
+                <li><a href="pengiriman.html">Jasa Kirim</a></li>
+                <li><a href="penyimpanan.html">Jasa Penyimpan</a></li>
+            </ul>
+        </div>
+
+        <div class="col-lg-3 col-md-12 footer-contact text-center text-md-start ">
+            <h4>Hubungi Kami</h4>
+            <p>
+                Bandung Techno Park, Gedung B No.108 <br> Bandung, Jawa Barat<br> Indonesia<br><br>
+                <strong>Phone:</strong> +62 811 2199 706<br>
+                <strong>Email:</strong> info@titipan.id<br>
+            </p>
+
+        </div>
+
+    </div>
+</div>
+
+<div class="container mt-4 ">
+    <div class="copyright ">
+        &copy; Copyright <strong><span>Titipan</span></strong>. All Rights Reserved
+    </div>
+</div>
+
+</footer>
+<!-- End Footer -->
+<!-- End Footer -->
---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,33 @@
+<!-- ======= Header ======= -->
+<header id="header" class="header d-flex align-items-center fixed-top">
+    <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
+
+        <a href="index.html" class="logo d-flex align-items-center">
+
+            <img src="{{ asset('images/titipanfix.png')}}" class="image" alt="">
+            <h1>Titipan</h1>
+        </a>
+
+        <i class="mobile-nav-toggle mobile-nav-show bi bi-list"></i>
+        <i class="mobile-nav-toggle mobile-nav-hide d-none bi bi-x"></i>
+        <nav id="navbar" class="navbar">
+            <ul>
+                <li><a href="index.html" class="active">Home</a></li>
+                <li>
+                    <a href="pindahan.html">Jasa Pindahan</a>
+                </li>
+                <li>
+                    <a href="penyimpanan.html">Jasa Penyimpan</a>
+                </li>
+                <li>
+                    <a href="pengiriman.html">Jasa Pengiriman</a>
+                </li>
+                <li><a href="contact.html">Contact</a></li>
+            </ul>
+        </nav>
+        <!-- .navbar -->
+
+    </div>
+</header>
+<!-- End Header -->
+<!-- End Header -->
