4444c5a9db00f87721f00a11a860c2f2f93174d0
Hanan Farizta <hananfariztaa@gmail.com>
2023-03-29 22:15:48 +0700

    feat/update route

--- resources/views/loginregister.blade.php
+++ resources/views/loginregister.blade.php
@@ -3,18 +3,18 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <script
-      src="https://kit.fontawesome.com/64d58efce2.js"
-      crossorigin="anonymous"
+    <script 
+    src="https://kit.fontawesome.com/64d58efce2.js" 
+    crossorigin="anonymous"
     ></script>
     <link rel="stylesheet" href="{{ asset('css/loginregister.css') }}" />
-    <title>Sign in & Sign up Form</title>
+    <title>Tiba-tiba Pindahan</title>
   </head>
-  <body></body>
+  <body>
     <div class="container">
       <div class="forms-container">
         <div class="signin-signup">
-          <form action="#" class="sign-in-form">
+          <form action="/" class="sign-in-form">
             <h2 class="title">Sign in</h2>
             <div class="input-field">
               <i class="fas fa-user"></i>
